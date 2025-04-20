67634e5c29acdb03cf7175a2138b9c315f396276
TelevisionNinja <TelevisionNinja@gmail.com>
2025-04-19 20:38:38 -0400

    clean up code

--- tests/testArrayUtils.py
+++ tests/testArrayUtils.py
@@ -1,6 +1,7 @@
 from src import arrayUtils
 import unittest
 
+
 class ArrayUtilsTest(unittest.TestCase):
     def test_array(self):
         self.assertTrue(arrayUtils.contains([0,1,2,3], 2))
@@ -9,6 +10,7 @@ class ArrayUtilsTest(unittest.TestCase):
         self.assertFalse(arrayUtils.contains([], 4))
         self.assertTrue(arrayUtils.zeros(2), [0,0])
         self.assertTrue(arrayUtils.ones(2), [1,1])
+
 
 if __name__ == '__main__':
     unittest.main()
