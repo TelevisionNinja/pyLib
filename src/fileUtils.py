ff12138f705aa0e42e456d879f5c519c186b12a3
TelevisionNinja <TelevisionNinja@gmail.com>
2025-05-24 09:38:05 -0400

    added zip creation and extraction

--- src/fileUtils.py
+++ src/fileUtils.py
@@ -1,5 +1,6 @@
 import tarfile
 import os
+import zipfile
 
 
 def extractTar(tar: tarfile.TarFile, path: str):
@@ -48,3 +49,53 @@ def createTar(dir_to_archive: str, archive_name: str, archive_path: str):
             fullFilePath = os.path.join(fullpath, file)
             # arcname is the relative path
             tar.add(fullFilePath, arcname=file)
+
+
+def extractZip(zip: zipfile.ZipFile, path: str, password: bytes = None):
+    """
+    extract TarFile relative to a path. only use this function if the zip was not created properly (using absolute paths instead of relative paths). use .extractall() if it was created properly
+
+    ex:
+    zip = ["/home/files/file1.txt", "/home/files/file2.txt"]
+    using extractZip(zip, "/extracted") = ["/extracted/file1.txt", "/extracted/file2.txt"]
+    using zip.extractall("/extracted") = ["/extracted/home/files/file1.txt", "/extracted/home/files/file2.txt"]
+    """
+
+    names = zip.namelist()
+    length = len(names)
+
+    if length == 0:
+        return
+
+    path = os.path.abspath(path)
+    common_path = ""
+
+    if length == 1: # get rid of parent directory
+        common_path = os.path.dirname(names[0])
+    else: # get rid of shared parent directory
+        common_path = os.path.commonpath(names)
+
+    for member in zip.infolist():
+        directory_split = member.filename.split(common_path, maxsplit=1)
+
+        if len(directory_split) > 1: # could be a file
+            member.filename = directory_split[1].lstrip(os.path.sep)
+
+        zip.extract(member, path, pwd=password)
+
+
+def createZip(dir_to_archive: str, archive_name: str, archive_path: str):
+    """
+    dir_to_archive: the directory of files and directories you want to be put in a zip
+    archive_name: the name of the zip (include the extension)
+    archive_path: the location of where you want to create the zip
+
+    python can't create encrypted zip files
+    """
+    with zipfile.ZipFile(os.path.join(archive_path, archive_name), "w") as zip:
+        fullpath = os.path.abspath(dir_to_archive)
+
+        for file in os.listdir(fullpath):
+            fullFilePath = os.path.join(fullpath, file)
+            # arcname is the relative path
+            zip.write(fullFilePath, arcname=file)
--- tests/testFileUtils.py
+++ tests/testFileUtils.py
@@ -2,6 +2,7 @@ from src import fileUtils
 import unittest
 import os.path
 import tarfile
+import zipfile
 import os
 
 
@@ -52,6 +53,53 @@ class FileUtilsTest(unittest.TestCase):
             extractedFileText = extractedFile.read()
         self.assertEqual(extractedFileText, "test")
         self.assertTrue(os.path.exists("./tarDirectory/"))
+
+    def test_zip_utils(self):
+        with zipfile.ZipFile("test.zip", "w") as zip:
+            zip.write("./tests/zipTest.txt")
+
+        with zipfile.ZipFile("test.zip") as zip:
+            fileUtils.extractZip(zip, "./")
+
+        extractedFileText = ""
+        with open(os.path.abspath("./zipTest.txt")) as extractedFile:
+            extractedFileText = extractedFile.read()
+        self.assertEqual(extractedFileText, "zip test")
+
+        os.remove("test.zip")
+        os.remove("./zipTest.txt")
+
+        with zipfile.ZipFile("test.zip", "w") as zip:
+            zip.write("./tests/zipTest.txt")
+            os.makedirs("./tests/zipDirectory/")
+            zip.write("./tests/zipDirectory/")
+
+        with zipfile.ZipFile("test.zip") as zip:
+            fileUtils.extractZip(zip, "./")
+
+        extractedFileText = ""
+        with open(os.path.abspath("./zipTest.txt")) as extractedFile:
+            extractedFileText = extractedFile.read()
+        self.assertEqual(extractedFileText, "zip test")
+        self.assertTrue(os.path.exists("./zipDirectory/"))
+
+        #-----------------------------------------
+
+        os.remove("test.zip")
+        os.remove("./zipTest.txt")
+        os.rmdir("./zipDirectory/")
+
+        fileUtils.createZip("./tests", "test.zip", "./")
+        self.assertTrue(os.path.exists("./test.zip"))
+
+        with zipfile.ZipFile("./test.zip") as zip:
+            zip.extractall(path="./")
+
+        extractedFileText = ""
+        with open(os.path.abspath("./zipTest.txt")) as extractedFile:
+            extractedFileText = extractedFile.read()
+        self.assertEqual(extractedFileText, "zip test")
+        self.assertTrue(os.path.exists("./zipDirectory/"))
 
 
 if __name__ == '__main__':
